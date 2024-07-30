082442098d3cda7592d26f167b3d06ac0bf5186f
Danylo Ilchyshyn <kruberlick@gmail.com>
2024-07-30 15:55:59 +0300

    Initial Commit

---
+++ TuneInStation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TuneInStation
+//
+//  Created by Danylo Ilchyshyn on 30.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TuneInStation/TuneInStationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuneInStationApp.swift
+//  TuneInStation
+//
+//  Created by Danylo Ilchyshyn on 30.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct TuneInStationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
